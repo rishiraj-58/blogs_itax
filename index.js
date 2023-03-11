42886951d719bdb7d0ebee01dc9c2229363fb4b2
rishiraj-58 <s.rishiraj2002@gmail.com>
2023-03-11 16:21:50 +0530

    admin can update and delete
    update can be done dynamically.

--- index.js
+++ index.js
@@ -85,9 +85,68 @@ app.post("/order", (req,res) => {
     })
 })
 
-app.put("/updateblog/:id", (req,res) => {
+app.put('/updateblog/:id', (req, res) => {
     const id = req.params.id;
-})
+    const {sn, section, sub_section, authority, won} = req.body;
+  
+    // Build the SET clause dynamically based on which properties are present in the request body
+    let setClause = '';
+    const values = [];
+    if (sn) {
+      setClause += 'sn = ?, ';
+      values.push(sn);
+    }
+    if (section) {
+      setClause += 'section = ?, ';
+      values.push(section);
+    }
+    if (sub_section) {
+      setClause += 'sub_section = ?, ';
+      values.push(sub_section);
+    }
+    if (authority) {
+        setClause += 'authority = ?, ';
+        values.push(authority);
+    }
+    if (won) {
+        setClause += 'won = ?, ';
+        values.push(won);
+      }
+    setClause = setClause.slice(0, -2); // remove the trailing comma and space
+  
+    const sql = `UPDATE blogs SET ${setClause} WHERE sn = ?`;
+  
+    // Add the id value to the end of the values array
+    values.push(id);
+  
+    connection.query(sql, values, (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error updating blog');
+      } else {
+        console.log(`Updated blog with id ${id}`);
+        res.status(200).send('blog updated successfully');
+      }
+    });
+  });
+
+// Delete a particular row in the "blogs" table
+app.delete('/deleteblog/:id', (req, res) => {
+    const id = req.params.id;
+// First, delete the rows in the `orders` table that reference the row to be deleted in the `blogs` table
+const deleteOrdersQuery = "DELETE FROM Orders WHERE id IN (SELECT id FROM blogs WHERE sn = ?)";
+connection.query(deleteOrdersQuery, [id], (error, results) => {
+  if (error) throw error;
+
+  // Next, delete the row from the `blogs` table
+  const deleteBlogQuery = "DELETE FROM blogs WHERE sn = ?";
+  connection.query(deleteBlogQuery, [id], (error, results) => {
+    if (error) throw error;
+    res.send(`Blog with sn ${id} deleted successfully`);
+  });
+});
+});
+
 
 app.listen(3300, () => {
     console.log('Server started on port 3300!');
